85d01ce4bb50537dbe532e1f7c7c105cf40f032b
Manuel Canero <manuel.canero@ovoenergy.com>
2019-05-23 22:59:23 +0100

    Added labels in options and default value

--- gmail-tester.js
+++ gmail-tester.js
@@ -1,13 +1,16 @@
 const gmail = require("./gmail");
 const fs = require("fs");
-const {
-  google
-} = require("googleapis");
+const { google } = require("googleapis");
 const util = require("util");
 
-async function _get_recent_email(credentials_json, token_path, options = {
-  include_body: true
-}) {
+async function _get_recent_email(
+  credentials_json,
+  token_path,
+  options = {
+    include_body: true,
+    label: "INBOX"
+  }
+) {
   const emails = [];
   // Load client secrets from a local file.
   const content = fs.readFileSync(credentials_json);
@@ -16,57 +19,72 @@ async function _get_recent_email(credentials_json, token_path, options = {
     version: "v1",
     oAuth2Client
   });
-  const gmail_emails = await gmail.get_recent_email(gmail_client, oAuth2Client);
-  for (const gmail_email of gmail_emails) {
-    const emailFrom = gmail_email.payload.headers.find(h => h.name === "From");
-    const emailSubject = gmail_email.payload.headers.find(h => h.name === "Subject");
-    const emailTo = gmail_email.payload.headers.find(h => h.name === "To");
-    const email = {
-      from: emailFrom ? emailFrom.value : "",
-      subject: emailSubject ? emailSubject.value : "",
-      receiver: emailTo ? emailTo.value : ""
-    };
-    if (options.include_body) {
-      let email_body = {
-        html: "",
-        text: ""
+  const gmail_emails = await gmail.get_recent_email(
+    gmail_client,
+    oAuth2Client,
+    options.label
+  );
+  if (gmail_emails && gmail_emails.length > 0) {
+    for (const gmail_email of gmail_emails) {
+      const emailFrom = gmail_email.payload.headers.find(
+        h => h.name === "From"
+      );
+      const emailSubject = gmail_email.payload.headers.find(
+        h => h.name === "Subject"
+      );
+      const emailTo = gmail_email.payload.headers.find(h => h.name === "To");
+      const email = {
+        from: emailFrom ? emailFrom.value : "",
+        subject: emailSubject ? emailSubject.value : "",
+        receiver: emailTo ? emailTo.value : ""
       };
-      const {
-        body
-      } = gmail_email.payload;
-      if (body.size) {
-        switch (gmail_email.payload.mimeType) {
-          case "text/html":
-            email_body.html = Buffer.from(body.data, "base64").toString("utf8");
-            break;
-          case "text/plain":
-          default:
-            email_body.text = Buffer.from(body.data, "base64").toString("utf8");
-            break;
-        }
-      } else {
-        let body_part = gmail_email.payload.parts.find(
-          p => p.mimeType === "text/html"
-        );
-        if (body_part) {
-          email_body.html = Buffer.from(body_part.body.data, "base64").toString(
-            "utf8"
+      if (options.include_body) {
+        let email_body = {
+          html: "",
+          text: ""
+        };
+        const { body } = gmail_email.payload;
+        if (body.size) {
+          switch (gmail_email.payload.mimeType) {
+            case "text/html":
+              email_body.html = Buffer.from(body.data, "base64").toString(
+                "utf8"
+              );
+              break;
+            case "text/plain":
+            default:
+              email_body.text = Buffer.from(body.data, "base64").toString(
+                "utf8"
+              );
+              break;
+          }
+        } else {
+          let body_part = gmail_email.payload.parts.find(
+            p => p.mimeType === "text/html"
           );
-        }
-        body_part = gmail_email.payload.parts.find(
-          p => p.mimeType === "text/plain"
-        );
-        if (body_part) {
-          email_body.text = Buffer.from(body_part.body.data, "base64").toString(
-            "utf8"
+          if (body_part) {
+            email_body.html = Buffer.from(
+              body_part.body.data,
+              "base64"
+            ).toString("utf8");
+          }
+          body_part = gmail_email.payload.parts.find(
+            p => p.mimeType === "text/plain"
           );
+          if (body_part) {
+            email_body.text = Buffer.from(
+              body_part.body.data,
+              "base64"
+            ).toString("utf8");
+          }
         }
-      }
 
-      email.body = email_body;
+        email.body = email_body;
+      }
+      emails.push(email);
     }
-    emails.push(email);
   }
+
   return emails;
 }
 
@@ -88,7 +106,11 @@ async function check_inbox(
     let found_email = null;
     let done_waiting_time = 0;
     do {
-      const emails = await _get_recent_email(credentials_json, token_path, options);
+      const emails = await _get_recent_email(
+        credentials_json,
+        token_path,
+        options
+      );
       for (let email of emails) {
         if (
           email.receiver.indexOf(to) >= 0 &&
